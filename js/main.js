54fd6b57a7eee2d1680913a9d80d24faf0e22604
dev-foysal <rtr.foysala@gmail.com>
2022-04-23 16:41:04 +0600

    construction

---
+++ js/main.js
@@ -0,0 +1,99 @@
+let navbar = document.querySelector('.header .navbar');
+let searchForm = document.querySelector('.header .search-form');
+let loginForm = document.querySelector('.header .login-form');
+let contactInfo = document.querySelector('.contact-info');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+};
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+};
+
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+};
+
+
+document.querySelector('#info-btn').onclick = () =>{
+    contactInfo.classList.add('active');
+ }
+ 
+ document.querySelector('#close-contact-info').onclick = () =>{
+    contactInfo.classList.remove('active');
+ }
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    contactInfo.classList.remove('active');
+}
+
+//home slider
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    grabCursor:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+ });
+
+ //review slider
+ var swiper = new Swiper(".reviews-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 20,
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        },
+      },
+ });
+
+ //blogs slider
+ var swiper = new Swiper(".blogs-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 20,
+    breakpoints: {
+       640: {
+         slidesPerView: 1,
+       },
+       768: {
+         slidesPerView: 2,
+       },
+       991: {
+         slidesPerView: 3,
+       },
+    },
+ });
+
+ //client logo slider
+ var swiper = new Swiper(".logo-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 20,
+    breakpoints: {
+       450: {
+          slidesPerView: 2,
+        },
+       640: {
+         slidesPerView: 3,
+       },
+       768: {
+         slidesPerView: 4,
+       },
+       1000: {
+         slidesPerView: 5,
+       },
+    },
+ });
